2a92cc3c6c784f35ac9e3722feb9ffaae0ca972c
Chris Warburton <chriswarbo@gmail.com>
2018-08-01 22:13:21 +0100

    Remove more nix-shell

---
+++ scripts/get_tag.nix
@@ -0,0 +1,27 @@
+{ bash, nixpkgs1709, wrap }:
+
+wrap {
+  name   = "get_tag";
+  paths  = [ bash nixpkgs1709.kid3 ];
+  script = ''
+    #!/usr/bin/env bash
+
+    export DISPLAY=:0
+
+    TAG="$1"
+
+    function getTag() {
+      NAME=$(basename "$1" | sed -e 's/"/\"/g')
+       DIR=$(dirname  "$1")
+
+      kid3-cli -c 'select "'"$NAME"'"' -c 'get "'"$TAG"'"' "$DIR"
+    }
+
+    shift
+
+    for ARG in "$@"
+    do
+      getTag "$ARG"
+    done
+  '';
+}
