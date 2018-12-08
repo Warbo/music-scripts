72d5c7991c455134e404441dda46ee912506905d
Chris Warburton <chriswarbo@gmail.com>
2018-12-08 21:36:14 +0000

    Only warn of crap when we've found some

--- scripts/delete_crap.sh
+++ scripts/delete_crap.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
-echo "Looking for crap you might want to delete" 1>&2
-
 DIR="$1"
 [[ -n "$DIR" ]] || DIR="Music"
 
-find "$DIR" -iname "*.db"      \
-        -or -iname "*.jpg"     \
-        -or -iname "*.jpeg"    \
-        -or -iname "*.png"     \
-        -or -iname "*.url"     \
-        -or -iname "*.txt"     \
-        -or -iname "*.ini"     \
-        -or -iname "*.onetoc2" \
-        -or -iname "*.htm"     \
-        -or -iname "*.html"
+PREFIXED=0
+
+while read -r P
+do
+    [[ -n "$P" ]] || continue
+    if [[ "$PREFIXED" -eq 0 ]]
+    then
+        PREFIXED=1
+        echo "Found crap you might want to delete" 1>&2
+    fi
+    echo "$P"
+done < <(find "$DIR" -iname "*.db"      \
+                 -or -iname "*.jpg"     \
+                 -or -iname "*.jpeg"    \
+                 -or -iname "*.png"     \
+                 -or -iname "*.url"     \
+                 -or -iname "*.txt"     \
+                 -or -iname "*.ini"     \
+                 -or -iname "*.onetoc2" \
+                 -or -iname "*.htm"     \
+                 -or -iname "*.html")
