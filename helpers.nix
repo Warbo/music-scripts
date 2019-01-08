f4f5144e6738901dc0d12ba9862de05cd2a45353
Chris Warburton <chriswarbo@gmail.com>
2019-01-08 08:39:24 +0000

    Bump helpers

--- helpers.nix
+++ helpers.nix
@@ -3,13 +3,13 @@
 {
   nix-helpers = fetchgit {
     url    = http://chriswarbo.net/git/nix-helpers.git;
-    rev    = "4fb7c87";
-    sha256 = "1wwagwxjj8v5q5wi75fcqz6xl17b1jf4l5p051vx371xj373v3dk";
+    rev    = "148bd5e";
+    sha256 = "0wywgdmv4gllarayhwf9p41pzrkvgs32shqrycv2yjkwz321w8wl";
   };
 
   warbo-packages = fetchgit {
     url    = http://chriswarbo.net/git/warbo-packages.git;
-    rev    = "6e4a804";
-    sha256 = "1aqp5qyvv47cpf16hyv5i41820hjymdbs3xr8czgd8hipjz601zz";
+    rev    = "e988092";
+    sha256 = "082ibmy2q9zvrm85bncm10v29rm53k25dwlgmqgmldfppprxcwja";
   };
 }
