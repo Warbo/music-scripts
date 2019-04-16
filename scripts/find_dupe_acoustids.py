2a7cd0e84db4af35af25c17d844fecf0ff371dd5
Chris Warburton <chriswarbo@gmail.com>
2019-04-16 17:53:50 +0100

    Allow find_dupe_acoustids to take paths as args

--- scripts/find_dupe_acoustids.py
+++ scripts/find_dupe_acoustids.py
@@ -1,23 +1,44 @@
 #!/usr/bin/env python3
 from os      import listdir
 from os.path import exists
-from sys     import stderr, stdout
+from sys     import argv, stderr, stdout
 
 msg  = lambda m: (stderr.write(m), stderr.flush())
 ids  = {}
 root = '.acoustid_cache'
 
+def strip(arg):
+    bits = arg.split('/')
+
+    assert bits[-4:-2] == ['Music', 'Commercial'], repr({
+        'error': 'Arguments should end in Music/Commercial/<init>/<artist>',
+        'arg'  : arg
+    })
+
+    return bits[-2:]
+
+given = list(map(strip, argv[1:]))
+if given == []:
+    inits   = listdir(root)
+    artists = lambda i: listdir(root + '/' + i)
+else:
+    msg('Limiting our search to the given paths ' + repr(given) + '\n')
+    inits   = list(set([arg[0] for arg in given]))
+    artists = lambda i: [arg[1] for arg in given if arg[0] == i]
+
 msg('Reading cached AcoustIDs')
-for init in listdir(root):
+for init in inits:
     d = root + '/' + init
-    for artist in listdir(d):
+    for artist in artists(init):
+        a = d + '/' + artist
         msg('.')
-        with open(d + '/' + artist, 'rb') as f:
+        with open(a, 'rb') as f:
             for line in f.readlines():
                 (path, duration, fingerprint) = line.split(b'\t')
                 if fingerprint not in ids:
                     ids[fingerprint] = []
-                ids[fingerprint].append((int(duration.decode('utf-8')), path))
+                    ids[fingerprint].append((int(duration.decode('utf-8')),
+                                             path))
 msg('\n')
 
 msg('Looking for duplicates.\n')
