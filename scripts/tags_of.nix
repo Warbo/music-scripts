56d6405ec65f9885ea04911077de5e0f4adc49d2
Chris Warburton <chriswarbo@gmail.com>
2019-04-16 13:46:17 +0100

    Wrap tags_of to force dependency

---
+++ scripts/tags_of.nix
@@ -0,0 +1,16 @@
+{ bash, nixpkgs1709, wrap }:
+
+wrap {
+  name   = "tags_of";
+  paths  = [ bash nixpkgs1709.kid3 ];
+  script = ''
+    #!/usr/bin/env bash
+
+    DIR=$(dirname  "$1")
+      F=$(basename "$1")
+
+    F_ESC="''${F//\"/\\\"}"
+
+    kid3-cli -c 'select "'"$F_ESC"'"' -c 'get all' "$DIR"
+  '';
+}
--- scripts/tags_of.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=$(dirname  "$1")
-F=$(basename "$1")
-
-F_ESC="${F//\"/\\\"}"
-
-kid3-cli -c 'select "'"$F_ESC"'"' -c 'get all' "$DIR"
