75a69708c137d23f794a586017cc37289651b35c
Chris Warburton <chriswarbo@gmail.com>
2019-04-16 17:20:01 +0100

    use mutagen-inspect in dodgy_looking_tags for non-MP3s

--- scripts/dodgy_looking_tags.nix
+++ scripts/dodgy_looking_tags.nix
@@ -1,33 +1,60 @@
-{ bash, pythonPackages, wrap }:
+{ bash, fail, pythonPackages, wrap }:
 
 wrap {
   name   = "dodgy_looking_tags";
-  paths  = [ bash pythonPackages.mutagen ];
+  paths  = [ bash fail pythonPackages.mutagen ];
   script = ''
     #!/usr/bin/env bash
 
-    function checkTags {
-      while read -r LINE
+    function go {
+      find "$1" -type f | while read -r F
       do
-        if printf "%s'" "$LINE" | grep " '$"
+        printf '.' 1>&2
+        TAGS=$(mutagen-inspect "$F")
+
+        if echo "$TAGS" | grep '\s$' > /dev/null
         then
+          echo
           echo "$F has tag ending in whitespace"
+          echo
+        fi
+
+        if echo "$TAGS" | grep '=\s' > /dev/null
+        then
+          echo
+          echo "$F has tag beginning with whitespace"
+          echo
         fi
 
         for DODGY in "music.download.com"
         do
-          if echo "$LINE" | grep -F "$DODGY"
+          if echo "$TAGS" | grep -F "$DODGY"
           then
-            echo "$1 has dodgy tag"
+            echo
+            echo "$F has dodgy tags"
+            echo
           fi
         done
       done
     }
 
-    find Music/Commercial -iname "*.mp3" | while read -r F
-    do
-      printf '.' 1>&2
-      mid3v2 --list "$F" | checkTags "$F"
-    done
+    if [[ "$#" -gt 0 ]]
+    then
+      DIR="$1"
+      [[ -d "$DIR" ]] || fail "Given directory '$DIR' not found, aborting"
+      go "$DIR"
+    else
+      DIR=Music/Commercial
+      [[ -d "$DIR" ]] || fail "Couldn't find '$DIR', maybe change working dir?"
+      for INIT in "$DIR"/*
+      do
+        [[ -d "$INIT" ]] || continue
+        for ARTIST in "$INIT"/*
+        do
+          [[ -d "$ARTIST" ]] || continue
+          go "$ARTIST"
+        done
+      done
+    fi
   '';
 }
