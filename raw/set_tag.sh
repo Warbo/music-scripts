45bd951840a52a79a2b901f2f1263b349948b57a
Chris Warburton <chriswarbo@gmail.com>
2019-04-23 01:31:53 +0100

    Check for known error messages when setting tags

--- raw/set_tag.sh
+++ raw/set_tag.sh
@@ -5,12 +5,17 @@ VAL="$2"
 
 function setTag() {
     NAME=$(basename "$1" | sed -e 's/"/\"/g')
-    DIR=$(dirname  "$1")
+     DIR=$(dirname  "$1")
 
     # shellcheck disable=SC2154
-    "$xvfb" kid3-cli -c 'select "'"$NAME"'"'        \
-                     -c 'set "'"$TAG"'" "'"$VAL"'"' \
-                     -c 'save' "$DIR"
+    GOT=$("$xvfb" kid3-cli -c 'select "'"$NAME"'"'        \
+                           -c 'set "'"$TAG"'" "'"$VAL"'"' \
+                           -c 'save' "$DIR" 2>&1)
+
+    if echo "$GOT" | grep 'Error while writing file' 1>&2
+    then
+        exit 1
+    fi
 }
 
 shift
