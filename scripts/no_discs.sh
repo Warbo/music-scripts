c89a6fc1d02ec24469292f8cd3abd47991b04ff8
Chris Warburton <chriswarbo@gmail.com>
2019-05-20 12:01:33 +0100

    Allow arguments to no_discs

--- scripts/no_discs.sh
+++ scripts/no_discs.sh
@@ -2,43 +2,56 @@
 
 # Try to get rid of things like "(Disc 1)" in directory names, as they just
 # lead to duplicates
-for LETTER in Music/Commercial/*
-do
-    [[ -d "$LETTER" ]] || continue
-    for ARTIST in "$LETTER"/*
+
+function checkArtist {
+    for ALBUM in "$1"/*
     do
-        for ALBUM in "$ARTIST"/*
+        [[ -d "$ALBUM" ]] || continue
+
+        NAME=$(basename "$ALBUM")
+        if echo "$NAME" | grep -i disc > /dev/null
+        then
+            echo "'$ALBUM' may be disc-specific album" >> /dev/stderr
+        fi
+
+        # Skip dodgy chars
+        echo "$NAME" | rev > /dev/null 2> /dev/null || continue
+
+         NODISC=$(echo "$NAME"   | rev | cut -c 10- | rev)
+          LOWER=$(echo "$NAME"   | tr '[:upper:]' '[:lower:]')
+        NOLOWER=$(echo "$NODISC" | tr '[:upper:]' '[:lower:]')
+        for DISC in 1 2 3 4 5
         do
-            [[ -d "$ALBUM" ]] || continue
-
-            NAME=$(basename "$ALBUM")
-            if echo "$NAME" | grep -i disc > /dev/null
+            if [[ "x${NOLOWER} (disc ${DISC})" = "x$LOWER" ]]
             then
-                echo "'$ALBUM' may be disc-specific album" >> /dev/stderr
+                DIR=$(dirname "$ALBUM")
+                pushd "$DIR" > /dev/null || exit 1
+                mkdir -p "$NODISC"
+                for TRACK in "$NAME"/*
+                do
+                     TRACKESC=$(echo "$PWD/$TRACK"   | esc.sh)
+                    NODISCESC=$(echo "$PWD/$NODISC/" | esc.sh)
+                    echo "mv -v '$TRACKESC' '$NODISCESC'"
+                done
+                popd > /dev/null || exit 1
             fi
-
-            # Skip dodgy chars
-            echo "$NAME" | rev > /dev/null 2> /dev/null || continue
-
-            NODISC=$(echo "$NAME" | rev | cut -c 10- | rev)
-            LOWER=$(echo "$NAME"     | tr '[:upper:]' '[:lower:]')
-            NOLOWER=$(echo "$NODISC" | tr '[:upper:]' '[:lower:]')
-            for DISC in 1 2 3 4 5
-            do
-                if [[ "x${NOLOWER} (disc ${DISC})" = "x$LOWER" ]]
-                then
-                    DIR=$(dirname "$ALBUM")
-                    pushd "$DIR" > /dev/null || exit 1
-                    mkdir -p "$NODISC"
-                    for TRACK in "$NAME"/*
-                    do
-                         TRACKESC=$(echo "$PWD/$TRACK"   | esc.sh)
-                        NODISCESC=$(echo "$PWD/$NODISC/" | esc.sh)
-                        echo "mv -v '$TRACKESC' '$NODISCESC'"
-                    done
-                    popd > /dev/null || exit 1
-                fi
-            done
         done
     done
-done
+}
+
+if [[ "$#" -eq 0 ]]
+then
+    for LETTER in Music/Commercial/*
+    do
+        [[ -d "$LETTER" ]] || continue
+        for ARTIST in "$LETTER"/*
+        do
+            checkArtist "$ARTIST"
+        done
+    done
+else
+    for D in "$@"
+    do
+        checkArtist "$D"
+    done
+fi
