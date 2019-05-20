fca55c1e4a5b60b0a692923e289d8368bf779871
Chris Warburton <chriswarbo@gmail.com>
2019-05-20 17:53:40 +0100

    Allow find_untagged to take args

--- scripts/find_untagged.sh
+++ scripts/find_untagged.sh
@@ -9,23 +9,35 @@ function hasSuffix() {
     fi
 }
 
-while read -r FILE
-do
-    if hasSuffix "mp3"  "$FILE" ||
-       hasSuffix "ogg"  "$FILE" ||
-       hasSuffix "oga"  "$FILE" ||
-       hasSuffix "opus" "$FILE" ||
-       hasSuffix "wma"  "$FILE" ||
-       hasSuffix "m4a"  "$FILE"
-    then
-        TAGS=$(tags_of "$FILE")
-        if echo "$TAGS" | grep -i 'title' > /dev/null
+function go {
+    while read -r FILE
+    do
+        if hasSuffix "mp3"  "$FILE" ||
+           hasSuffix "ogg"  "$FILE" ||
+           hasSuffix "oga"  "$FILE" ||
+           hasSuffix "opus" "$FILE" ||
+           hasSuffix "wma"  "$FILE" ||
+           hasSuffix "m4a"  "$FILE"
         then
-            continue
+            TAGS=$(tags_of "$FILE")
+            if echo "$TAGS" | grep -i 'title' > /dev/null
+            then
+                continue
+            fi
+            printf 'Untagged\t'\''%s'\''\n' "$(echo "$FILE" | esc.sh)"
+            echo "$TAGS" 1>&2
+        else
+            echo "Don't know format of '$FILE'" 1>&2
         fi
-        printf 'Untagged\t'\''%s'\''\n' "$(echo "$FILE" | esc.sh)"
-        echo "$TAGS" 1>&2
-    else
-        echo "Don't know format of '$FILE'" 1>&2
-    fi
-done < <(find Music/Commercial -type f)
+    done < <(find "$1" -type f)
+}
+
+if [[ "$#" -eq 0 ]]
+then
+    go "Music/Commercial"
+else
+    for DIR in "$@"
+    do
+        go "$DIR"
+    done
+fi
