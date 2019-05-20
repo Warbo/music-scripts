ac1e6b3f498e527de4fda2de6a12e8353d170542
Chris Warburton <chriswarbo@gmail.com>
2019-05-20 17:23:17 +0100

    Let youtube names take arg

--- scripts/strip_youtube_names.sh
+++ scripts/strip_youtube_names.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-while read -r F
-do
-    # shellcheck disable=SC2001
-    STRIPPED=$(echo "$F" | sed -e 's/-[^ ()]*\.\(....*\)/.\1/g')
+function go {
+    while read -r F
+    do
+        # shellcheck disable=SC2001
+        STRIPPED=$(echo "$F" | sed -e 's/-[^ ()]*\.\(....*\)/.\1/g')
 
-           F_ESC=$(echo "$F"        | esc.sh)
-    STRIPPED_ESC=$(echo "$STRIPPED" | esc.sh)
+               F_ESC=$(echo "$F"        | esc.sh)
+        STRIPPED_ESC=$(echo "$STRIPPED" | esc.sh)
 
-    echo "mv '$F_ESC' '$STRIPPED_ESC'"
-done < <(find Music/Commercial -type f | grep -- '-[^ ]*\....[.]*')
+        echo "mv '$F_ESC' '$STRIPPED_ESC'"
+    done < <(find "$1" -type f | grep -- '-[^ ]*\....[.]*')
+}
+
+if [[ "$#" -eq 0 ]]
+then
+    go Music/Commercial
+else
+    for D in "$@"
+    do
+        go "$D"
+    done
+fi
