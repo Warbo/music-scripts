976240b0acf0e1f82995d11648164237bee43da5
Chris Warburton <chriswarbo@gmail.com>
2019-11-29 09:45:13 +0000

    Support m4a in get_tag

--- raw/get_tag.py
+++ raw/get_tag.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 import mutagen
 import mutagen.easyid3
+import mutagen.mp4
 import sys
+
+# MP4 gives 'key error' if we try looking up tags like 'album', so we must swap
+# them out for their literal byte sequences.
+mp4Replacements = {
+    'title' : b'\xa9nam',
+    'album' : b'\xa9alb',
+    'artist': b'\xa9ART'
+}
 
 tag = sys.argv[1]
 
@@ -9,6 +18,8 @@ for f in sys.argv[2:]:
     try:
         audio = mutagen.easyid3.EasyID3(f)
     except:
+        if f.lower().endswith('m4a') and tag in mp4Replacements:
+            tag   = mp4Replacements[tag]
         audio = mutagen.File(f)
     try:
         print(audio[tag][0])
