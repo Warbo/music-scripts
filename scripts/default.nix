43f50356f3047d3a827e5c9240954bfa73af4b80
Chris Warburton <chriswarbo@gmail.com>
2023-10-01 17:52:46 +0100

    Allow scripts to reference warbo-utilities

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,11 +1,12 @@
-{ newScope, nix-helpers, nixpkgs-lib, warbo-packages }:
+{ newScope, nix-helpers, nixpkgs-lib, warbo-packages, warbo-utilities }:
 with rec {
   inherit (nix-helpers) nixDirsIn;
   inherit (nixpkgs-lib) mapAttrs;
 
   call = _: f:
-    newScope (nix-helpers // warbo-packages // { inherit music-scripts; }) f
-    { };
+    newScope (nix-helpers // warbo-packages // {
+      inherit music-scripts warbo-utilities;
+    }) f { };
 
   music-scripts = mapAttrs call (nixDirsIn {
     dir = ./.;
